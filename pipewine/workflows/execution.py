4df9e200100dafce25d4a7200c32367debaaf936
LucaBonfiglioli <luca.bonfiglioli@gmail.com>
2025-01-13 20:30:10 +0100

    Allow checkpoints in workflows (wip)

--- pipewine/workflows/execution.py
+++ pipewine/workflows/execution.py
@@ -1,14 +1,19 @@
 from abc import ABC, abstractmethod
-from collections.abc import Mapping, Sequence
+from collections.abc import Callable, Mapping, Sequence
 from functools import partial
+from pathlib import Path
 from typing import cast
+from uuid import uuid1
 
 from pipewine._op_typing import AnyDataset
 from pipewine.bundle import Bundle
 from pipewine.dataset import Dataset
-from pipewine.operators import DatasetOperator
-from pipewine.sinks import DatasetSink
-from pipewine.sources import DatasetSource
+from pipewine.operators import CacheOp, DatasetOperator
+from pipewine.sample import Sample
+from pipewine.sinks import DatasetSink, UnderfolderSink
+from pipewine.sources import DatasetSource, UnderfolderSource
+from tempfile import gettempdir
+from pipewine.grabber import Grabber
 from pipewine.workflows.model import AnyAction, Node, Proxy, Workflow
 from pipewine.workflows.tracking import (
     EventQueue,
@@ -29,10 +34,43 @@ class WorkflowExecutor(ABC):
     def detach(self) -> None: ...
 
 
+class CheckpointFactory(ABC):
+    @abstractmethod
+    def create[
+        T: Sample
+    ](
+        self, execution_id: str, name: str, sample_type: type[T], grabber: Grabber
+    ) -> tuple[DatasetSink[Dataset[T]], DatasetSource[Dataset[T]]]: ...
+
+
+class UnderfolderCheckpointFactory(CheckpointFactory):
+    def __init__(self, folder: Path | None = None) -> None:
+        self._folder = folder or Path(gettempdir()) / "pipewine_workflows"
+
+    def create[
+        T: Sample
+    ](
+        self, execution_id: str, name: str, sample_type: type[T], grabber: Grabber
+    ) -> tuple[DatasetSink[Dataset[T]], DatasetSource[Dataset[T]]]:
+        path = self._folder / execution_id / name
+        sink = UnderfolderSink(path, grabber=grabber)
+        source = UnderfolderSource(path, sample_type=sample_type)
+        return sink, source
+
+
 class SequentialWorkflowExecutor(WorkflowExecutor):
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        checkpoint_factory: CheckpointFactory | None = None,
+        checkpoint_grabber: Grabber | None = None,
+        cache: CacheOp | None = None,
+    ) -> None:
         super().__init__()
         self._eq: EventQueue | None = None
+        self._checkpoint_factory = checkpoint_factory
+        self._checkpoint_grabber = checkpoint_grabber or Grabber()
+        self._cache = cache
+        self._id = uuid1()
 
     def attach(self, event_queue: EventQueue) -> None:
         if self._eq is not None:
@@ -109,17 +147,35 @@ class SequentialWorkflowExecutor(WorkflowExecutor):
         if output is None:
             return
         if isinstance(output, Dataset):
-            state[Proxy(node, None)] = output
+            self._handle_output(state, Proxy(node, None), output)
         elif isinstance(output, Sequence):
             for i, dataset in enumerate(output):
-                state[Proxy(node, i)] = dataset
+                self._handle_output(state, Proxy(node, i), dataset)
         elif isinstance(output, Mapping):
             for k, v in output.items():
                 state[Proxy(node, k)] = v
+                self._handle_output(state, Proxy(node, k), v)
         else:
             assert isinstance(output, Bundle)
             for k, v in output.as_dict().items():
-                state[Proxy(node, k)] = v
+                self._handle_output(state, Proxy(node, k), v)
+
+    def _handle_output(
+        self, state: dict[Proxy, Dataset], proxy: Proxy, dataset: Dataset
+    ) -> None:
+        if self._checkpoint_factory is not None and len(dataset) > 0:
+            sink, source = self._checkpoint_factory.create(
+                self._id.hex,
+                proxy.node.name,
+                type(dataset[0]),
+                self._checkpoint_grabber,
+            )
+            sink.register_on_enter(partial(self._on_enter_cb, proxy.node))
+            sink.register_on_iter(partial(self._on_iter_cb, proxy.node))
+            sink.register_on_exit(partial(self._on_exit_cb, proxy.node))
+            sink(dataset)
+            dataset = source()
+        state[proxy] = dataset
 
     def _topological_sort(self, workflow: Workflow) -> list[Node]:
         result: list[Node] = []
