a0c4e2661acdc63bcec0d1f4977daa2873bf2f9a
LucaBonfiglioli <luca.bonfiglioli@gmail.com>
2025-02-15 14:10:06 +0100

    Add -m option to CLI to import extension modules

--- pipewine/cli/__init__.py
+++ pipewine/cli/__init__.py
@@ -1,6 +1,7 @@
+from pipewine.cli.extension import import_module
 from pipewine.cli.main import pipewine_app
 from pipewine.cli.mappers import map_cli
 from pipewine.cli.ops import op_cli
 from pipewine.cli.sinks import sink_cli
 from pipewine.cli.sources import source_cli
-from pipewine.cli.utils import run_cli_workflow, parse_grabber, parse_sink, parse_source
+from pipewine.cli.utils import parse_grabber, parse_sink, parse_source, run_cli_workflow
--- pipewine/cli/main.py
+++ pipewine/cli/main.py
@@ -5,18 +5,26 @@ from typer import Option, Typer
 from pipewine.cli.mappers import map_app
 from pipewine.cli.ops import op_app
 from pipewine.cli.workflows import wf_app
+from pipewine.cli.extension import import_module
+
+version_help = "Show the current Pipewine installation version."
+module_help = (
+    "Add an extension module: path to python script, module path, or plain python code."
+)
 
 
 def main_callback(
-    version: Annotated[
-        bool, Option(help="Show pipewine version", is_eager=True)
-    ] = False,
+    version: Annotated[bool, Option(help=version_help, is_eager=True)] = False,
+    module: Annotated[list[str], Option(..., "-m", "--module", help=module_help)] = [],
 ) -> None:
     from pipewine import __version__
 
     if version:
         print(__version__)
         exit(0)
+
+    for m in module:
+        import_module(m)
 
 
 pipewine_app = Typer(
